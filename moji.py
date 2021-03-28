6d088a215078aa7dee8d07148c3854e9b07ea53a
yum <savvyyum7119@protonmail.ch>
2021-03-28 22:20:54 +0800

    Add files via upload

--- moji.py
+++ moji.py
@@ -3,14 +3,13 @@ from discord.ext import commands
 
 import utils.config as MOJI
 import utils.tools as TOOLS
- 
+
 ##########################################################
 ##                                                      ##
 ##          MOJI the new whoelesome emoji/bot           ##
 ##                                                      ##
 ##########################################################
 
-#   moji
 moji = commands.Bot(
     command_prefix=TOOLS.get_prefix,
     description=MOJI.description,
@@ -19,9 +18,10 @@ moji = commands.Bot(
     owner_id=622733200917004309,
     status=discord.Status.idle,
     activity=discord.Activity(type=discord.ActivityType.watching, name="evryone"))
- 
-TOOLS.log(moji)
+
+TOOLS.print_log(moji)
 TOOLS.load_cogs(moji)
 TOOLS.load_manager(moji)
+TOOLS.load_errorhandler(moji)
 
 moji.run(TOOLS.classified('token'))
