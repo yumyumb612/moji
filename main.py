7d75fd9bc403eba67ed0795ef4a46f150e97beb4
kshly <yumyum_b612@protonmail.ch>
2021-07-07 17:42:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,106 +5,123 @@ from platform import python_version
 
 import discord
 import pymongo
-from discord.ext import commands
+from discord.ext.commands import AutoShardedBot
+from discord_components import DiscordComponents
+from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
-import configs.config as config
-
+import configs.settings as settings
+import helpers.functions.loggers as logger
+import helpers.functions.tools as tool
+from helpers.database.mongodb import collection
 
 cwd= os.path.dirname(__file__)
 load_dotenv(dotenv_path=os.path.join(cwd, "configs/.evn"), override=True)
-class butt(commands.AutoShardedBot):
+class butt(AutoShardedBot):
     def __init__(self):
         super().__init__(
-            command_prefix= config.default_prefix,
-            description= config.description,
+            command_prefix= tool.set_prefix,
+            description= settings.description,
             status= discord.Status.dnd,
             activity= discord.Game(
                 name= "Vivifying. please wait",
                 start= datetime.utcnow(),
                 type= discord.ActivityType.unknown
             ),
-            allowed_mentions= config.allowed_mentions,
+            allowed_mentions= settings.allowed_mentions,
             case_insensitive= True,
-            intents= config.intents,
-            owner_ids= config.owner_ids,
+            intents= settings.intents,
+            owner_ids= settings.owner_ids,
             strip_after_prefix= True
         )
+        #   interactions
+        SlashCommand(self, sync_commands=True, sync_on_cog_reload=True, override_type=True)
+        DiscordComponents(self)
 
         #   attributes
         self.boot_datetime= datetime.utcnow()
         self.cwd: str= cwd
-        self.commands_completed= self.command_errors= self.self_errors= 0
+        self.commands_completed= self.commands_run= self.command_errors= self.self_errors= 0
+        self.blocked_users, self.recent_users= [], []
         self.versions= {
-            "bot": config.bot_version,
+            "bot": settings.bot_version,
             "dpy": f"{discord.__version__} {discord.version_info.releaselevel}",
             "py": python_version()
         }
-        self.modules= {
+        self.cog_counts= {
             "all": [],
-            "loaded": []
+            "loaded": [],
+            "commands": [],
+            "listeners": []
         }
 
         #   logging
-        print(f"{datetime.now()}: setting up logger...")
+        print(f"{datetime.now()}: Setting up logging...")
         # logging.basicConfig(level=logging.INFO)
 
-        discord_logger = logging.getLogger("discord")
-        discord_logger.setLevel(logging.INFO)
-        discord_handler = logging.FileHandler(filename= f"{self.cwd}/logs/discord.log", encoding="utf-8", mode="w")
-        discord_handler.setFormatter(logging.Formatter(config.logging_format))
+        discord_logger= logging.getLogger("discord")
+        discord_logger.setLevel(logging.DEBUG)
+        discord_handler= logging.FileHandler(filename= f"{self.cwd}/logs/discord.log", encoding= "utf-8", mode= "w")
+        discord_handler.setFormatter(logging.Formatter(settings.logging_format))
         discord_logger.addHandler(discord_handler)
 
-        self.bot_events_logger = logging.getLogger("bot_events")
-        self.bot_events_logger.setLevel(logging.INFO)
-        bot_events_handler = logging.FileHandler(filename= f"{self.cwd}/logs/bot_events.log", encoding="utf-8", mode="a")
-        bot_events_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        self.bot_events_logger.addHandler(bot_events_handler)
-        
-        self.modules_logger = logging.getLogger("modules")
-        self.modules_logger.setLevel(logging.INFO)
-        modules_handler = logging.FileHandler(filename= f"{self.cwd}/logs/modules.log", encoding="utf-8", mode="a")
-        modules_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        self.modules_logger.addHandler(modules_handler)
-        print(f"{datetime.now()}: done setting up logger")
+        self.butt_logger= logger.butt
+        self.event_logger= logger.event
+        self.command_logger= logger.command
+
+        logger.butt.log(20, "Started logging")
+        print(f"{datetime.now()}: Done setting logging")
 
         # database
-        print(f"{datetime.now()}: connecting to da database...")
-        mongodb_client = pymongo.MongoClient(os.getenv("mongodb_connection_string"), serverSelectionTimeoutMS=20000)
-        self.db = mongodb_client["mojidb"]
-        # self.guild_info = collection(self.db, "guild_info")
-        # self.features_data = collection(self.db, "features_data")
+        print(f"{datetime.now()}: Connecting to MongoDB database...")
         try:
+            mongodb_client= pymongo.MongoClient(os.getenv("mongodb_connection_string"), serverSelectionTimeoutMS=20000)
+            self.db= mongodb_client["mojidb"]
+            self.guild_settings= collection(self.db, "guild_settings")
+
             # mongodb_client.server_info()
-            print(f"{datetime.now()}: connected to da database")
+            print(f"{datetime.now()}: Connected to the database")
         except Exception as error:
             print(f"{datetime.now()}: {error}\n")
+
+        #   cogs
+        print(f"{datetime.now()}: Loading the cog(s)...")
+        cog_paths= [f"{self.cwd}/cogs/commands", f"{self.cwd}/cogs/listeners"]
+        def load_check(filename):
+            return filename.endswith('.py') and not filename.startswith("_")
         
-        #   modules
-        print(f"{datetime.now()}: loading module(s)...")
-        modules_paths = [f"{self.cwd}/modules/cogs", f"{self.cwd}/modules/events"]
-        for path in modules_paths:
+        self.load_extension("helpers.managers.cog_manager")
+
+        for path in cog_paths:
             for filename in os.listdir(path):
                 if filename == "__pycache__":
                     continue
                 else:
-                    self.modules["all"].append(filename.replace(".py", ""))
-                    if filename.endswith('.py') and not filename.startswith("_"):
-                        if path == modules_paths[0]:
-                            self.load_extension(f"modules.cogs.{filename.replace('.py', '')}")
+                    self.cog_counts["all"].append(filename.replace(".py", ""))
+                    if path == cog_paths[0]:
+                        self.cog_counts["commands"].append(filename.replace(".py", ""))
+                        if load_check(filename):
+                            self.load_extension(f"cogs.commands.{filename.replace('.py', '')}")
                         else:
-                            self.load_extension(f"modules.events.{filename.replace('.py', '')}")
+                            continue
                     else:
-                        continue
-        print(f"{datetime.now()}: loaded {len(self.modules['loaded'])}/{len(self.modules['all'])} module(s)")
+                        self.cog_counts["listeners"].append(filename.replace(".py", ""))
+                        if load_check(filename):
+                            self.load_extension(f"cogs.listeners.{filename.replace('.py', '')}")
+                        else:
+                            continue
+        print(f"{datetime.now()}: Loaded {len(self.cog_counts['loaded'])}/{len(self.cog_counts['all'])} cog(s)")
+        self.butt_logger
 
 if __name__ == "__main__":
     try:
         os.system("clear")
-        print(f"{datetime.now()}: running {cwd}/main.py")
+        print(f"{datetime.now()}: Running {cwd}/main.py")
         butt().run(os.getenv("discord_token"))
     except KeyboardInterrupt:
-        print("KeyboardInterrupt")
+        logger.butt.exception("Booting got cancelled")
+        print(f"\n{datetime.now()}: Booting got cancelled")
         butt().close()
     finally:
-        print(f"\n{datetime.now()}: KeyboardInterrupt")
+        logger.butt.log(50, "Closed all running loops")
+        print(f"\n{datetime.now()}: Closed all running loops")
