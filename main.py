daafefacaea4a4972a124d492ad3faa7b97d3ff5
kshly <yumyum_b612@protonmail.ch>
2021-07-15 10:28:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import helpers.functions.loggers as logger
 import helpers.functions.tools as tool
 from helpers.database.mongodb import collection
 
-cwd= os.path.dirname(__file__)
+cwd= tool.get_path()
 load_dotenv(dotenv_path=os.path.join(cwd, "configs/.evn"), override=True)
 class butt(AutoShardedBot):
     def __init__(self):
@@ -36,15 +36,18 @@ class butt(AutoShardedBot):
         )
         #   attributes
         self.boot_datetime= datetime.utcnow()
+        self.start_datatime= start_datatime
         self.cwd: str= cwd
+        self.mood= "cheeky"
+        self.colours= settings.colours
         self.commands_completed, self.commands_run, self.command_errors, self.self_errors= 0, 0, 0, 0
-        self.blocked_users, self.recent_users= [], []
+        self.ignored_users, self.recent_users= [], []
         self.versions= {
             "bot": settings.bot_version,
             "dpy": f"{discord.__version__} {discord.version_info.releaselevel}",
             "py": python_version()
         }
-        self.cog_counts= {
+        self.moji_cogs= {
             "all": [],
             "loaded": [],
             "commands": [],
@@ -60,7 +63,7 @@ class butt(AutoShardedBot):
         # logging.basicConfig(level=logging.INFO)
 
         discord_logger= logging.getLogger("discord")
-        discord_logger.setLevel(logging.DEBUG)
+        discord_logger.setLevel(logging.INFO)
         discord_handler= logging.FileHandler(filename= f"{self.cwd}/logs/discord.log", encoding= "utf-8", mode= "w")
         discord_handler.setFormatter(logging.Formatter(settings.logging_format))
         discord_logger.addHandler(discord_handler)
@@ -69,23 +72,28 @@ class butt(AutoShardedBot):
         self.event_logger= logger.event
         self.command_logger= logger.command
 
-        logger.butt.log(20, "Logging started")
+        self.butt_logger.log(20, "[Started logging]")
         print(f"{datetime.now()}: Done setting logger")
 
         # database
         print(f"{datetime.now()}: Connecting to MongoDB database...")
         try:
             mongodb_client= pymongo.MongoClient(os.getenv("mongodb_connection_string"), serverSelectionTimeoutMS=20000)
-            self.db= mongodb_client["mojidb"]
-            self.guild_settings= collection(self.db, "guild_settings")
+            self.db= mongodb_client["testdb"]
+            self.local_settings= collection(self.db, "local_settings")
+            self.local_function_settings= collection(self.db, "local_function_settings")
 
             # mongodb_client.server_info()
-            print(f"{datetime.now()}: Connected to the database")
+            # database= mongodb_client.list_database_names()
+            database= [1,2,3]
+            print(f"{datetime.now()}: Connected to the database. Found {len(database)}")
+            self.butt_logger.log(20, f"[Connected to MongoDB database] Databases: {database}")
         except Exception as error:
             print(f"{datetime.now()}: {error}\n")
+            self.butt_logger.log(40, f"[MongoDB Error] {error}")
 
         #   cogs
-        print(f"{datetime.now()}: Loading the cog(s)...")
+        print(f"{datetime.now()}: Loading cog(s)...")
         cog_paths= [f"{self.cwd}/cogs/commands", f"{self.cwd}/cogs/listeners"]
         def load_check(filename):
             return filename.endswith('.py') and not filename.startswith("_")
@@ -97,31 +105,35 @@ class butt(AutoShardedBot):
                 if filename == "__pycache__":
                     continue
                 else:
-                    self.cog_counts["all"].append(filename.replace(".py", ""))
+                    self.moji_cogs["all"].append(filename.replace(".py", ""))
                     if path == cog_paths[0]:
-                        self.cog_counts["commands"].append(filename.replace(".py", ""))
+                        self.moji_cogs["commands"].append(filename.replace(".py", ""))
                         if load_check(filename):
                             self.load_extension(f"cogs.commands.{filename.replace('.py', '')}")
                         else:
                             continue
                     else:
-                        self.cog_counts["listeners"].append(filename.replace(".py", ""))
+                        self.moji_cogs["listeners"].append(filename.replace(".py", ""))
                         if load_check(filename):
                             self.load_extension(f"cogs.listeners.{filename.replace('.py', '')}")
                         else:
                             continue
-        print(f"{datetime.now()}: Loaded {len(self.cog_counts['loaded'])}/{len(self.cog_counts['all'])} cog(s)")
-        self.butt_logger.log(20, f"Loaded {len(self.cog_counts['loaded'])}/{len(self.cog_counts['all'])} cog(s)")
+        print(f"{datetime.now()}: Loaded {len(self.moji_cogs['loaded'])}/{len(self.moji_cogs['all'])} cog(s)")
+        self.butt_logger.log(20, f"[Cog] All: `{len(self.moji_cogs['all'])}` Loaded: `{len(self.moji_cogs['loaded'])}` Commands: `{len(self.moji_cogs['commands'])}` Listeners: `{len(self.moji_cogs['listeners'])}`")
 
 if __name__ == "__main__":
     try:
         os.system("clear")
+        logger.butt.log(20, "[Script] Started running 'main.py'")
         print(f"{datetime.now()}: Running {cwd}/main.py")
+        start_datatime= datetime.utcnow()
         butt().run(os.getenv("discord_token"))
     except KeyboardInterrupt:
-        logger.butt.exception("Booting got cancelled")
+        logger.butt.log(40, "[Script] KeyboardInterrupt")
         print(f"\n{datetime.now()}: Booting got cancelled")
         butt().close()
+    except discord.errors.LoginFailure:
+        print(f"\n{datetime.now()}: Improper token has been passed")
     finally:
-        logger.butt.log(50, "Closed all running loops. Client offline")
-        print(f"\n{datetime.now()}: Closed all running loops. Client offline")
+        logger.butt.log(50, "[Script] Stoped running 'main.py'")
+        print(f"\n{datetime.now()}: Closed all the loops. Client offline")
